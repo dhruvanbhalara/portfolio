b3d2253327b76ee78b9e17e21e151b4739595698
Dhruvan Bhalara <dhruvan2912@yahoo.com>
2021-05-21 11:41:09 +0530

    Remove # from url
    Update dependency

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/pages/portfolioApp.dart';
 import 'package:provider/provider.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'providers/utility_provider.dart';
 
 void main() {
+  setPathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MultiProvider(
